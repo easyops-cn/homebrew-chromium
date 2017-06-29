a056e5b3a9de0aac65eec8df0196955b9065d87f
J Huang <i@jhuang.me>
2017-06-29 14:43:25 +0800

    fix(depot_tools): pass brew test depot_tools

--- Formula/depot_tools.rb
+++ Formula/depot_tools.rb
@@ -7,28 +7,28 @@ require "formula"
 class DepotTools < Formula
   homepage 'https://dev.chromium.org/developers/how-tos/install-depot-tools'
   url 'https://chromium.googlesource.com/chromium/tools/depot_tools.git', :branch => 'master'
-  version 'master'
+  version '2017.06.29'
 
   depends_on 'repo'
 
   def install
-    dst = prefix/'tools'
+    dst = prefix
     dst.mkpath unless dst.directory?
     mv Dir.glob('*'), dst
     %w[gclient gcl git-cl hammer drover cpplint.py presubmit_support.py
-      trychange.py git-try wtf weekly git-gs zsh-goodies].each do |tool|
-      (bin/tool).write <<-EOS.undent
-        #!/bin/bash
-        TOOL=#{prefix}/#{tool}
-        export DEPOT_TOOLS_UPDATE=0
-        export PATH="$PATH:#{prefix}"
-        exec "$TOOL" "$@"
-      EOS
-    end
+          git-try git-gs git-hyper-blame zsh-goodies].each do |tool|
+          (bin/tool).write <<-EOS.undent
+            #!/bin/bash
+            TOOL=#{prefix}/#{tool}
+            export DEPOT_TOOLS_UPDATE=0
+            export PATH="$PATH:#{prefix}"
+            exec "$TOOL" "$@"
+          EOS
+        end
   end
 
   test do
-    %w[gclient presubmit_support.py trychange.py].each do |tool|
+    %w[gclient presubmit_support.py].each do |tool|
       system "#{bin}/#{tool} --version"
     end
   end
